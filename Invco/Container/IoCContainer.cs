4bbdfaaefd52b28cc44056f1d489279dc57476f7
isurusiri <isuruanatomy@gmail.com>
2016-08-02 15:04:52 +0530

    ioc container initial implementation

---
+++ Invco/Container/IoCContainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invco.Container
+{
+    internal static class IoCContainer
+    {
+        static readonly IDictionary<Type, Type> Types = new Dictionary<Type, Type>();
+
+        public static void Register<TContract, TImplementation>()
+        {
+            Types[typeof (TContract)] = typeof (TImplementation);
+        }
+
+        public static T Resolve<T>()
+        {
+            return (T) Resolve(typeof (T));
+        }
+
+        public static object Resolve(Type contract)
+        {
+            Type implementation = Types[contract];
+            ConstructorInfo constructor = implementation.GetConstructors()[0];
+            ParameterInfo[] constructorParameters = constructor.GetParameters();
+
+            if (constructorParameters.Length == 1)
+            {
+                return Activator.CreateInstance(implementation);
+            }
+
+            List<object> parameters = new List<object>(constructorParameters.Length);
+            parameters.AddRange(constructorParameters.Select(parameterInfo => Resolve(parameterInfo.ParameterType)));
+
+            return constructor.Invoke(parameters.ToArray());
+        }
+    }
+}
