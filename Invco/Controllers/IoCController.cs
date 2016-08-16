a68b840fc8f172ab7ddc601b6eecf3ce6323250f
isurusiri <isuruanatomy@gmail.com>
2016-08-16 12:04:20 +0530

    public interface release

---
+++ Invco/Controllers/IoCController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Invco.Container;
+
+namespace Invco.Controllers
+{
+    public class IoCController
+    {
+        public static void Register<TContract, TImplementation>()
+        {
+            IoCContainer.Register<TContract, TImplementation>();
+        }
+
+        public static T Resolve<T>()
+        {
+            return IoCContainer.Resolve<T>();
+        }
+    }
+}
