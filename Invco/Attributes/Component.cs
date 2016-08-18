bf67f2b7c8d2b33aade71141248418c14186b099
isurusiri <isuruanatomy@gmail.com>
2016-08-18 09:42:05 +0530

    added component attribute

---
+++ Invco/Attributes/Component.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invco.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Component : Attribute
+    {
+    }
+}
