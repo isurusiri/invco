20de6e0a475bec958c9b196c1dfc31de5ea4e408
isurusiri <isuruanatomy@gmail.com>
2016-08-18 09:38:36 +0530

    added inject attribute

---
+++ Invco/Attributes/Inject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invco.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class Inject : Attribute
+    {
+        public Type Contract { get; set; }
+        public Type Implementation { get; set; }
+    }
+}
