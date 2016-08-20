5ee68e82c8f6d83fe814bc23059a32648d55e672
isurusiri <isuruanatomy@gmail.com>
2016-08-20 18:47:36 +0530

    removed unused imports

--- Invco/General/Scopes.cs
+++ Invco/General/Scopes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Invco.General
+﻿namespace Invco.General
 {
     public enum Scopes { Singleton, Prototype, Request, Session}
 }
