49bc80bb62a57894c3699dbf00b152bce85c8ff6
isurusiri <isuruanatomy@gmail.com>
2016-08-26 10:48:22 +0530

    registering dependencies

--- Invco/Core/Context/ContextInitializer.cs
+++ Invco/Core/Context/ContextInitializer.cs
@@ -5,14 +5,31 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Invco.Attributes;
+using Invco.Core.Container;
 
 namespace Invco.Core.Context
 {
     internal class ContextInitializer
     {
-        List<Type> GetAllComponents(Assembly sourceAssembly)
+        public List<Type> GetAllComponents(Assembly sourceAssembly)
         {
             return sourceAssembly.GetTypes().Where(type => type.GetCustomAttributes(typeof (Component), true).Length > 0).ToList();
+        }
+
+        public void RegisterDependency(Type sourceType)
+        {
+            foreach (var property in sourceType.GetProperties())
+            {
+                var attr = (Inject[])property.GetCustomAttributes(typeof(Inject), false);
+                if (attr.Length > 0)
+                {
+                    foreach (var inject in attr)
+                    {
+                        // have a possiblity of getting duplicated entries. to be decide.
+                        IoCContainer.Register(inject.Contract, inject.Implementation);
+                    }
+                }
+            }
         }
     }
 }
