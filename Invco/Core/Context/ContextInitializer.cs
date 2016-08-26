17bb1063acf4b2adef17154ea3238252f4bb93bd
isurusiri <isuruanatomy@gmail.com>
2016-08-26 10:26:36 +0530

    component scan initial source

--- Invco/Core/Context/ContextInitializer.cs
+++ Invco/Core/Context/ContextInitializer.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Invco.Attributes;
 
 namespace Invco.Core.Context
 {
     internal class ContextInitializer
     {
-
+        List<Type> GetAllComponents(Assembly sourceAssembly)
+        {
+            return sourceAssembly.GetTypes().Where(type => type.GetCustomAttributes(typeof (Component), true).Length > 0).ToList();
+        }
     }
 }
